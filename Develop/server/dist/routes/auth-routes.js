1938159fa8d1ed5d4f2473434f5ef307af4e9611
Yang Gao <yanggao413@gmail.com>
2024-11-23 21:23:25 -0500

    added precompiled dist for render

---
+++ Develop/server/dist/routes/auth-routes.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { User } from '../models/user.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+export const login = async (req, res) => {
+    // TODO: If the user exists and the password is correct, return a JWT token
+    const { username, password } = req.body;
+    const user = await User.findOne({
+        where: { username }
+    });
+    if (!user) {
+        return res.status(401).send('Invalid username or password');
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+        return res.status(401).send('Invalid username or password');
+    }
+    const jwtSecret = process.env.JWT_SECRET_KEY || '';
+    const token = jwt.sign({ id: user.id }, jwtSecret, {
+        expiresIn: '1h',
+    });
+    return res.json({ token });
+};
+const router = Router();
+// POST /login - Login a user
+router.post('/login', login);
+export default router;
